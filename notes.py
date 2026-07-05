0b23ab01fac30f258f1e326966d03d3e967f91fe
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2026-07-05 00:22:42 -0600

    up down window

--- notes.py
+++ notes.py
@@ -64,26 +64,40 @@ while running:
                 cursor[0] = min(len(text[cursor[1]]), cx+1)
             elif event.key == pygame.K_UP:
                 cy = cursor[1]
+                cursor[1] = max(0, cy-1)
+                cursor[0] = min(len(text[cursor[1]]), cursor[0])
+                wflag = 0
                 if cy == window_y[0]+3 and cy > 0:
                     if window_y[0] > 0:
                         window_y[0] -= 1
                         window_y[1] -= 1
-                        w_text = [wt[window_x[0]:window_x[1]] for wt in text[window_y[0]:window_y[1]]]
-                        all_text = '\n'.join(w_text)
-                        text_surface = font.render(all_text, False, text_color)
-                cursor[1] = max(0, cy-1)
-                cursor[0] = min(len(text[cursor[1]]), cursor[0]) 
+                        wflag = 1
+                if len(text[cursor[1]]) < len(text[cy]):
+                    window_x[0] = max(len(text[cursor[1]]) - ((WIDTH//cw)-1), 0)
+                    window_x[1] = max(len(text[cursor[1]]), (WIDTH//cw)-1)
+                    wflag = 1
+                if wflag == 1:
+                    w_text = [wt[window_x[0]:window_x[1]] for wt in text[window_y[0]:window_y[1]]]
+                    all_text = '\n'.join(w_text)
+                    text_surface = font.render(all_text, False, text_color)
             elif event.key == pygame.K_DOWN:
                 cy = cursor[1]
                 cursor[1] = min(len(text)-1, cy+1)
+                cursor[0] = min(len(text[cursor[1]]), cursor[0])
+                wflag = 0
                 if cy == window_y[1]-3 and cy > 0:
                     if window_y[1] < len(text):
                         window_y[0] += 1
                         window_y[1] += 1
-                        w_text = [wt[window_x[0]:window_x[1]] for wt in text[window_y[0]:window_y[1]]]
-                        all_text = '\n'.join(w_text)
-                        text_surface = font.render(all_text, False, text_color)
-                cursor[0] = min(len(text[cursor[1]]), cursor[0]) 
+                        wflag = 1
+                if len(text[cursor[1]]) < len(text[cy]):
+                    window_x[0] = max(len(text[cursor[1]]) - ((WIDTH//cw)-1), 0)
+                    window_x[1] = max(len(text[cursor[1]]), (WIDTH//cw)-1)
+                    wflag = 1
+                if wflag == 1:
+                    w_text = [wt[window_x[0]:window_x[1]] for wt in text[window_y[0]:window_y[1]]]
+                    all_text = '\n'.join(w_text)
+                    text_surface = font.render(all_text, False, text_color)
             elif event.key == pygame.K_BACKSPACE:
                 if cursor[0] > 0 and text[cursor[1]]:
                     text[cursor[1]] = text[cursor[1]][:cursor[0]-1] + text[cursor[1]][cursor[0]:]
